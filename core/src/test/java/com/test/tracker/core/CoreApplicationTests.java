7601497ad859a41f05d8a5b994771fc053cee555
Dmytro <dmytro.zhelezniak@proarea.co>
2021-02-08 14:46:02 +0200

    add core module
    - set database configuration
    - add core Dockerfile
    - update docker-compose.yml

---
+++ core/src/test/java/com/test/tracker/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tracker.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
