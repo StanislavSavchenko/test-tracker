99aacfebfe08a3910e4c10b3fcf9ba044cd462c7
Dmytro <dmytro.zhelezniak@proarea.co>
2021-02-09 15:01:15 +0200

    apply stash

--- core/src/main/java/com/test/tracker/core/CoreApplication.java
+++ core/src/main/java/com/test/tracker/core/CoreApplication.java
@@ -4,15 +4,47 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.test.tracker"})
+@EnableEurekaClient
+@EnableSwagger2
 public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.test.tracker.core.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("", "", "");
+        return new ApiInfoBuilder()
+                .contact(contact)
+                .title("Core API")
+                .description("")
+                .version("1.0")
+                .build();
+    }
 }
 
--- report/src/main/java/com/test/tracker/ReportLauncher.java
+++ report/src/main/java/com/test/tracker/ReportLauncher.java
@@ -3,13 +3,43 @@ package com.test.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class ReportLauncher {
 
     public static void main(String[] args) {
         SpringApplication.run(ReportLauncher.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.test.tracker.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("", "", "");
+        return new ApiInfoBuilder()
+                .contact(contact)
+                .title("Report API")
+                .description("")
+                .version("1.0")
+                .build();
+    }
 }
