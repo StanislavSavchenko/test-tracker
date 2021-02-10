3634975634c4d4242382f1022e6d2080882d154d
Dmytro <dmytro.zhelezniak@proarea.co>
2021-02-10 18:12:54 +0200

    apply swagger

--- core/src/main/java/com/test/tracker/core/config/SwaggerConfig.java
+++ core/src/main/java/com/test/tracker/core/config/SwaggerConfig.java
@@ -1,66 +1,31 @@
 package com.test.tracker.core.config;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.servlet.ServletContext;
-
-import static springfox.documentation.builders.PathSelectors.regex;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
-
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
-    public Docket api(ServletContext servletContext) {
+    public Docket swaggerPersonApi10() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(apis())
-                .paths(paths())
+                .apis(RequestHandlerSelectors.basePackage("com.test.tracker.core.controller"))
+                .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false);
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .version("1.0")
+                                .title("Core Service API")
+                                .description("Core Service API v1.0")
+                                .build()
+                );
     }
-
-    private Predicate<String> paths() {
-        return Predicates.or(
-                regex(".*")
-        );
-    }
-
-
-    private Predicate<RequestHandler> apis() {
-        return Predicates.and(
-                basePackage("com.test.tracker.core.controller"),
-                withClassAnnotation(Api.class)
-        );
-    }
-
-    private ApiInfo apiInfo() {
-        Contact contact = new Contact("", "", "");
-        return new ApiInfoBuilder()
-                .contact(contact)
-                .title("Core API")
-                .description("")
-                .version("1.0")
-                .build();
-    }
-
-
 }
